dd2b33bef01a82d1bd8d695986ea05915b49dd0c
Kysan721 <kyshu.senpai@gmail.com>
2019-05-25 17:35:25 +0200

    amélioration

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import os
+import sys
 
-print("started...")
+
 
 osu_path = "C:\\Games\\osu!"
 songs_dir = osu_path + "\\Songs"
-bmaps  = [x for x in os.listdir(songs_dir) if x != "fr.txt"]        # la langue peux varier edit ça
+beatmaps_path  = [x for x in os.listdir(songs_dir) if x != "fr.txt"]        # la langue peux varier edit ça
 
 
 def check_extension(fileName, ext):
     return fileName.split('.')[-1:][0] == ext
 
-print(check_extension("caca.py", "py"))
 
-for bmap in bmaps:
+def editAR(osuFile, newAR):
+    with open(osuFile, "rw"):
+        """
+        parse le fichier trouve la ligne ou il est écrit
+        ApproachRate: X
+        la modifie en changant X par newAR
+        """
+
+
+
+for bmp in beatmaps_path:
     #print(bmap)
 
-    difficulty = [m for m in os.listdir(songs_dir + "\\" + bmap) if check_extension(m, "osu")]
+    # seulement les fichiers en .osu nous interesse
+    difficulty = [d for d in os.listdir(songs_dir + "\\" + bmp) if check_extension(d, "osu")]
+    # il y en a une par difficulté
     print(difficulty)
+
+
+
+# peut être faire un cli
